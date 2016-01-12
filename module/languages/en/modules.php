a11c38d8fd0efaf0a802b3fc5b4da2a3b5b3273c
David Molineus <david.molineus@netzmacht.de>
2016-01-12 17:52:27 +0100

    Add translations.

---
+++ module/languages/en/modules.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package    contao-feature-toggle
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2016 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+$GLOBALS['TL_LANG']['MOD']['feature-toggle'][0] = 'Feature Toggle';
+$GLOBALS['TL_LANG']['MOD']['feature-toggle'][1] = 'Feature Toggling';
---
+++ module/languages/en/tl_page.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package    contao-feature-toggle
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2016 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+$GLOBALS['TL_LANG']['tl_page']['feature_toggle'][0] = 'Toggle feature';
+$GLOBALS['TL_LANG']['tl_page']['feature_toggle'][1] = 'Choose a toggle feature which is used as visibility condition.';
