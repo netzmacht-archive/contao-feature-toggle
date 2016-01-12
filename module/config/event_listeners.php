fbfce244d4518ed9074815c793105885704aef77
David Molineus <david.molineus@netzmacht.de>
2016-01-12 16:35:00 +0100

    Add file header.

--- module/config/event_listeners.php
+++ module/config/event_listeners.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2016 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
 use Netzmacht\Contao\FeatureToggle\Event\InitializeDefaultContextEvent;
 use Netzmacht\Contao\FeatureToggle\Event\InitializeToggleCollectionEvent;
 
